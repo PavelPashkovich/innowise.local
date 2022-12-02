72feeaf910388ab457a7672941ac9567b9413a31
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-12-02 18:55:19 +0300

    add Bootstrap

--- app/views/main/notFound.php
+++ app/views/main/notFound.php
@@ -3,8 +3,13 @@ $page_title = "Page not found";
 require_once __DIR__ . '/../header_layout.php';
 ?>
 
-<div class="wrapper">
-    <h2>404 Page not found!</h2>
+<div class="container">
+    <div class="row justify-content-center vh-100 align-items-center">
+        <div>
+            <h1 class="text-center">404 Page not found!</h1>
+        </div>
+
+    </div>
 </div>
 
 <?php require_once __DIR__ . '/../footer_layout.php'; ?>
--- app/views/users/show.php
+++ app/views/users/show.php
@@ -1,49 +1,53 @@
 <?php
-$page_title = "Users";
+$page_title = "User page";
 require_once __DIR__ . '/../header_layout.php';
 require_once __DIR__ . '/../header_nav.php';
 ?>
 
-<div class="wrapper">
-
-    <?php if (isset($error)): ?>
-        <h3><?php echo $error; ?></h3>
-    <?php else: ?>
-    <?php if (isset($users)): ?>
-    <table>
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Gender</th>
-            <th>Status</th>
-            <th colspan="3">Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><?php echo $users['id']; ?></td>
-                <td><?php echo $users['name']; ?></td>
-                <td><?php echo $users['email']; ?></td>
-                <td><?php echo $users['gender']; ?></td>
-                <td><?php echo $users['status']; ?></td>
-                <td>
-                    <div class="actions-container">
-                        <form action="edit/<?php echo $users['id']; ?>" method="get">
-                            <button>Edit</button>
-                        </form>
-                        <form onsubmit="delete_confirm(event)" action="<?php echo $users['id']; ?>" method="post">
-                            <button type="submit">Delete</button>
-                        </form>
+<div class="container">
+    <div class="row justify-content-center vh-100 align-items-center">
+        <div class="col-xl-9 col-lg-10 col-md-11 col-sm-12">
+            <?php if (isset($error)): ?>
+                <h3><?php echo $error; ?></h3>
+            <?php else: ?>
+                <?php if (isset($users)): ?>
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead class="text-center bg-light">
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Gender</th>
+                                <th>Status</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody class="text-center align-middle">
+                            <tr>
+                                <td><?php echo $users['id']; ?></td>
+                                <td class="text-start"><?php echo $users['name']; ?></td>
+                                <td class="text-start"><?php echo $users['email']; ?></td>
+                                <td><?php echo $users['gender']; ?></td>
+                                <td><?php echo $users['status']; ?></td>
+                                <td>
+                                    <div class="container d-inline-flex justify-content-center">
+                                        <form role="form" class="form-container" action="edit/<?php echo $users['id']; ?>" method="get">
+                                            <button class="btn btn-outline-warning m-1" type="submit"><i class="bi bi-pencil-square"></i> Edit</button>
+                                        </form>
+                                        <form role="form" class="form-container" onsubmit="delete_confirm(event)" action="<?php echo $users['id']; ?>" method="post">
+                                            <button class="btn btn-outline-danger m-1" type="submit"><i class="bi bi-trash"></i> Delete</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <?php endif; ?>
-    <?php endif; ?>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
-
 
 <?php require_once __DIR__ . '/../footer_layout.php'; ?>
